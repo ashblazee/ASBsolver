6d28e9554adace21ac3a4a4e371092387618f50c
ashblazee <166774717+ashblazee@users.noreply.github.com>
2024-11-11 22:05:20 +0800

    Add files via upload

---
+++ Finite Difference Method/premake5.lua
@@ -0,0 +1,16 @@
+workspace "ASBsolver"
+    configurations { "Debug", "Release" }
+    platforms {"x64"}
+
+
+project "FDM example"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    files {"FDM.cpp","example.cpp"}
+
+    filter "configurations:Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        optimize "On"
