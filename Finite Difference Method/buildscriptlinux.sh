0092bccb4cb14ad067bec32e84d7283d73ebbf80
ashblazee <166774717+ashblazee@users.noreply.github.com>
2024-11-17 21:02:47 +0800

    Update buildscriptlinux.sh

--- Finite Difference Method/buildscriptlinux.sh
+++ Finite Difference Method/buildscriptlinux.sh
@@ -4,9 +4,9 @@ echo -n "1)GNUMake 2)VS2022"
 read userInput
 
 if [ "$userInput" == "1" ]; then
-    premake5 gmake
+    ./premake5 gmake
 elif [ "$userInput" == "2" ]; then
-    premake5 vs2022
+    ./premake5 vs2022
 else
     echo "Invalid input. Please enter either 1 or 2."
 fi
